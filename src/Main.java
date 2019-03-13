1939365005fd115ceeb3f735bc605d1e2511064b
Korumer <drazkowski1999@gmail.com>
2019-03-13 11:27:20 +0100

    Dodanie funkcji NWD

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main
     {
         System.out.println( Color.BLUE + "Hello world" + Color.DEFAULT );
         System.out.println( Color.GREEN + Silnia.result(5) + Color.DEFAULT );
+        System.out.println( Color.CYAN + NWD.result(7, 6)+ Color.DEFAULT);
     }
 }
