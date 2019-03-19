66b2338f7037512c7d7f4b7ce0efb5275f1781a9
Martyna <tarczewskamartyna@gmail.com>
2019-03-19 16:47:12 +0100

    final version

---
+++ Application/src/Exercise3.java
@@ -0,0 +1,11 @@
+public class Exercise3 {
+    static int result(int a)
+    {
+        if (a==0)
+            return 1;
+        else
+        {
+           return a * result(a-1);
+        }
+    }
+}
