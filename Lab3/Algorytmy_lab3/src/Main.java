18579a6323937c47d54ba562e936497df929b822
Martyna <tarczewskamartyna@gmail.com>
2019-03-26 10:54:50 +0100

    ex3, ex5, ex6, ex7

--- Lab3/Algorytmy_lab3/src/Main.java
+++ Lab3/Algorytmy_lab3/src/Main.java
@@ -1,57 +1,77 @@
 import java.util.Scanner;
 
 public class Main {
-    private static boolean isRunning = true;
+  private static boolean isRunning = true;
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        do {
-            Scanner in = new Scanner(System.in);
-            System.out.println();
-            System.out.println("Choose an option:");
-            System.out.println("[1] - ");
-            System.out.println("[2] - ");
-            System.out.println("[3] - ");
-            System.out.println("[4] - ");
-            System.out.println("[5] - ");
-            System.out.println("[6] - ");
-            System.out.println("[7] - ");
-            System.out.println("[8] - Exit");
-            System.out.print("Your choice: ");
+    do {
+      Scanner in = new Scanner(System.in);
+      System.out.println();
+      System.out.println("Choose an option:");
+      System.out.println("[1] - ");
+      System.out.println("[2] - ");
+      System.out.println("[3] - Fibonacci Sequence");
+      System.out.println("[4] - ");
+      System.out.println("[5] - Guess the number");
+      System.out.println("[6] - Determinants method");
+      System.out.println("[7] - ");
+      System.out.println("[8] - Exit");
+      System.out.print("Your choice: ");
 
-            short response = in.nextShort();
+      short response = in.nextShort();
 
-            switch (response) {
+      switch (response) {
 
-                case 1:
-                    break;
+        case 1:
+          break;
 
-                case 2:
-                    break;
+        case 2:
+          break;
 
-                case 3:
-                    break;
+        case 3:
+          int n;
+          System.out.println("Enter the number of fibonacci sequence elements(1-92): ");
+          n = in.nextInt();
+          Exercise3.fibonacciSeqence(n);
+          break;
 
-                case 4:
-                    break;
+        case 4:
+          break;
 
-                case 5:
-                    break;
+        case 5:
+          Exercise5.checkNumber();
+          break;
 
-                case 6:
-                    break;
+        case 6:
+          System.out.println("General form of equations: " + "\n" + "ax + by = A" + "\n" + "cx + dy = B");
+          System.out.print("Enter a: ");
+          double a = in.nextDouble();
+          System.out.print("Enter b: ");
+          double b = in.nextDouble();
+          System.out.print("Enter A: ");
+          double A = in.nextDouble();
+          System.out.print("Enter c: ");
+          double c = in.nextDouble();
+          System.out.print("Enter d: ");
+          double d = in.nextDouble();
+          System.out.print("Enter B: ");
+          double B = in.nextDouble();
 
-                case 7:
-                    break;
+          Exercise6.doMath(a, b, c, d, A, B);
+          break;
 
-                case 8:
-                    System.out.println(Color.BLUE + "Bye :P" + Color.DEFAULT);
-                    isRunning = false;
-                    break;
+        case 7:
+          break;
 
-                default:
-                    break;
-            }
-        } while (isRunning);
-    }
+        case 8:
+          System.out.println(Color.BLUE + "Bye :P" + Color.DEFAULT);
+          isRunning = false;
+          break;
+
+        default:
+          break;
+      }
+    } while (isRunning);
+  }
 }
