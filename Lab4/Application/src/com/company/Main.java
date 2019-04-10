009a6710e9b36f22ef6932e24b1e259e72a4b2e4
Martyna <tarczewskamartyna@gmail.com>
2019-04-10 20:09:56 +0200

    ex 2 and 5

--- Lab4/Application/src/com/company/Main.java
+++ Lab4/Application/src/com/company/Main.java
@@ -12,10 +12,10 @@ public class Main {
             System.out.println();
             System.out.println("Choose an option:");
             System.out.println("[1] - ");
-            System.out.println("[2] - ");
+            System.out.println("[2] - Change of numbers from binary and decimal systems: ");
             System.out.println("[3] - ");
             System.out.println("[4] - ");
-            System.out.println("[5] - ");
+            System.out.println("[5] - Check if the given string is a palindrome. ");
             System.out.println("[6] - ");
             System.out.println("[7] - Exit");
             System.out.print("Your choice: ");
@@ -28,6 +28,7 @@ public class Main {
                     break;
 
                 case 2:
+                    Exercise2.menu();
                     break;
 
                 case 3:
@@ -37,6 +38,9 @@ public class Main {
                     break;
 
                 case 5:
+                    System.out.println("Enter string: ");
+                    String word = in.next();
+                    Exercise5.checkPalindrome(word);
                     break;
 
                 case 6:
