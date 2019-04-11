d6946a5678d9f9ff5db45c15cfb1e536cd60d769
Martyna <tarczewskamartyna@gmail.com>
2019-04-11 18:17:55 +0200

    ex 4

--- Lab4/Application/src/com/company/Exercise4.java
+++ Lab4/Application/src/com/company/Exercise4.java
@@ -1,21 +1,27 @@
 package com.company;
+
 import java.util.Scanner;
 
 class Exercise4 {
-    static void weighted_average(int n){
-        Scanner input = new Scanner(System.in);
-        int s1=0, s2=0, no, w;
+  static void weighted_average(int n) {
+    Scanner input = new Scanner(System.in);
+    int s1 = 0, s2 = 0, no, w;
 
-        for(int i = 1; i<=n; i++){
-            System.out.print("enter the number: ");
-            no = input.nextInt();
-            System.out.print("and her weight: ");
-            w = input.nextInt();
-            s1+=no*w;
-            s2+=w;
-        }
-
-        System.out.println("Weighted average of given numbers: " + (double)s1/s2);
+    for (int i = 1; i <= n; i++) {
+      System.out.print("enter the number: ");
+      no = input.nextInt();
+      System.out.print("and her weight: ");
+      w = input.nextInt();
+      s1 += no * w;
+      s2 += w;
     }
+
+    if (s2 == 0) {
+      System.out.println("Dividing by 0 - try again");
+    } else {
+
+      System.out.println("Weighted average of given numbers: " + (double) s1 / s2);
+    }
+  }
 
 }
