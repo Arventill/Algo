544e86a90fad43d6ff5154c58b3ed80096afe6b9
Filip Nowicki <39906425+arventill@users.noreply.github.com>
2019-05-04 18:13:25 +0200

    Update selectionSort.cpp

--- Lab6/Code/selectionSort.cpp
+++ Lab6/Code/selectionSort.cpp
@@ -1,1 +1,51 @@
-heh
+#include <iostream>
+#include <fstream>
+
+const int ELEMENTS = 4;
+int t[ELEMENTS]{4,3,2,1};
+
+void fill_tab()
+{
+	using namespace std;
+	for (int i = 0; i < ELEMENTS; i++)
+	{
+		cin >> t[i];
+	}
+}
+
+void show_tab()
+{
+	using namespace std;
+	for (int i = 0; i < ELEMENTS; i++)
+	{
+		cout << t[i] << endl;
+	}
+}
+
+int main()
+{
+	using namespace std;
+	int counting = 0, mini, ch;
+
+	//fill_tab();
+	
+	for (int i = 0; i < ELEMENTS; i++)
+	{
+		ch = i;
+		mini = t[counting];
+		for (auto j = counting; j < ELEMENTS; j++)
+		{
+			if (t[j] < mini)
+			{
+				mini = t[j];
+				ch = j;
+			}
+		}
+		swap(t[counting], t[ch]);
+		counting++;
+	}
+
+	show_tab();
+
+	return 0;
+}
