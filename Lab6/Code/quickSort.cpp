92d797d7672d6a8cba466ba2c38a577c59a0ffa9
Filip Nowicki <39906425+arventill@users.noreply.github.com>
2019-05-04 16:46:48 +0200

    Update quickSort.cpp

--- Lab6/Code/quickSort.cpp
+++ Lab6/Code/quickSort.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <ctime>
 #include <vector>
+
 #define SIZE 30
+
 using namespace std;
-void quick_sort(vector<int>& numbers_vector, const int left_pivot, const int
-	right_pivot) {
+
+void quick_sort(vector<int>& numbers_vector, const int left_pivot, const int right_pivot) 
+{
 	const auto pivot = numbers_vector[(left_pivot + right_pivot) / 2];
 	auto i = left_pivot, j = right_pivot;
-	while (i <= j) {
+
+	while (i <= j) 
+	{
 		while (numbers_vector[i] < pivot) ++i;
 		while (numbers_vector[j] > pivot) --j;
-		if (i <= j) {
+		if (i <= j) 
+		{
 			swap(numbers_vector[i], numbers_vector[j]);
 			++i, --j;
 		}
@@ -24,11 +30,15 @@ void quick_sort(vector<int>& numbers_vector, const int left_pivot, const int
 		quick_sort(numbers_vector, left_pivot, j);
 	}
 }
-void sort(vector<int>& numbers_vector) {
-	if (!numbers_vector.empty()) {
+
+void sort(vector<int>& numbers_vector) 
+{
+	if (!numbers_vector.empty()) 
+	{
 		quick_sort(numbers_vector, 0, numbers_vector.size() - 1);
 	}
 }
+
 int main()
 {
 	vector<int> numbers_vector;
@@ -38,13 +48,13 @@ int main()
 	{
 		numbers_vector.push_back(rand() % 1500);
 	}
-	//Code to check how algorithm handles with same values
-	//numbers_vector.push_back(numbers_vector[0]);
+
 	sort(numbers_vector);
 	cout << "Zakonczono sortowanie. Tablica po posortowaniu:" << endl;
 	for (auto& i : numbers_vector)
 	{
 		cout << i << endl;
 	}
+
 	return 0;
 }
